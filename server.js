792a919ac46f07335afe9023f0ebda20ae7c990d
Devji090 <144488365+Devji090@users.noreply.github.com>
2023-10-11 22:37:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV  || 'development';
-const express = require('./config/express');
+var express = require('./config/express');
 
-const app = express();
+var app = express();
 
 app.listen(2002);
 
